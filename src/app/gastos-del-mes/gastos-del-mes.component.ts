57e6d66bf89af05217102ea4ef8c231666c8f488
Ernesto Serravalle <eserravalle@velocitypartners.net>
2017-09-29 12:38:33 -0300

    Listo: > permitir que el usuario pueda elegir el mes (el usuario hace click para consultar el mes) > no permitir que se pueda consultar la base de datos si el usuario no está logueado

--- src/app/gastos-del-mes/gastos-del-mes.component.ts
+++ src/app/gastos-del-mes/gastos-del-mes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Gasto } from '../model/gasto';
 import { GastoRepositoryService } from '../services/gasto-repository.service';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-gastos-del-mes',
@@ -10,13 +11,25 @@ import { GastoRepositoryService } from '../services/gasto-repository.service';
 export class GastosDelMesComponent implements OnInit {
 
   gastosDelMes: Array<Gasto>;
+  meses: Array<string>;
+  mes: string;
+  consultarButtonEnabled: boolean;
 
-  constructor(private gastoRepositoryService: GastoRepositoryService) {
+  constructor(private gastoRepositoryService: GastoRepositoryService, private loginService: LoginService) {
     this.gastosDelMes = new Array<Gasto>();
+    this.meses = ['2017-08', '2017-09'];
+    this.mes = '2017-09';
+    this.consultarButtonEnabled = false;
   }
 
   ngOnInit() {
-    this.gastoRepositoryService.obtenerGastosDelMes('2017-09')
+    this.loginService.loggedIn.subscribe((value) => {
+      this.consultarButtonEnabled = value;
+    });
+  }
+
+  consultarGastosDelMes() {
+    this.gastoRepositoryService.obtenerGastosDelMes(this.mes)
     .then((gastos) => this.gastosDelMes = gastos)
     .catch((error) => alert(`${error.message} No se han podido recuperar los Gastos del Mes. ¡Intente nuevamente!`));
   }
